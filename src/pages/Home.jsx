3f915b557eb3deaf83d8aee946e22b5a47524219
arpita-32 <arpitasundaray9@gmail.com>
2024-12-16 20:45:06 +0530

    updated styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import Spinner from '../components/Spinner';
-import Product from '../components/Product';
+import { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
 
 const Home = () => {
-    const API_URL = "https://fakestoreapi.com/products";
-    const [loading, setLoading] = useState(false);
-    const [posts, setPosts] = useState([]);
-    
-    async function fetchProductDate(){
-        setLoading(true);
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
 
-        try{
-            const res = await fetch(API_URL);
-            const data = await res.json();
+  async function fetchProductData() {
+    setLoading(true);
 
-            setPosts(data);
-        }
-        catch(error){
-            console.log("Error");
-            setPosts([]);
+    try{
+      const res = await fetch(API_URL);
+      const data = await res.json();
 
-        }
-        setLoading(false);
+      setPosts(data);
     }
-    useEffect( () => {
-        fetchProductDate();
-    },[])
+    catch(error) {
+      console.log("Error aagya ji");
+      setPosts([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect( () => {
+    fetchProductData();
+  },[])
+
   return (
     <div>
-    {
-        loading ? <Spinner/> :
-        posts.length > 0 ?
-        (<div>
-            {
-                posts.map((post) => (
-                    <Product key={post.id} post={post}/>
-                ))
-            }
-            </div>):
-        <div>
-            <p>
-                NO DATA FOUND
-            </p>
-        </div>
-    }
+      {
+        loading ? <Spinner />  :
+        posts.length > 0 ? 
+        (<div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+          {
+            posts.map( (post) => (
+            <Product key = {post.id} post={post}/>
+          ) )
+          }
+        </div>) :
+        <div className="flex justify-center items-center">
+          <p>No Data Found</p>
+        </div> 
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
